b1d8fd86008d96356e41e6d0da8651aac6eb97f5
LaggyTurtle <chasenmon5@gmail.com>
2025-01-23 11:43:59 -1000

    first thing

---
+++ src/main/java/laggys/minecraft/mod/LaggysMod.java
@@ -0,0 +1,16 @@
+package laggys.minecraft.mod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LaggysMod implements ModInitializer {
+	public static final String MOD_ID = "laggys-mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/laggys/minecraft/mod/LaggysModClient.java
@@ -0,0 +1,10 @@
+package laggys.minecraft.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LaggysModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
